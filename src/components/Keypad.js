a8bfca02ff2863aa664f38b8841cea4e011d5863
John J Setlock II <johnsetlock@gmail.com>
2022-08-12 07:35:50 -0500

    Completed Assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
 
+
+
 function Keypad (){
+    function usingKeypad(){
+        console.log('Entering password...')
+    }
     return (
-        <div></div>
+        <div>
+            <input onChange={usingKeypad} type="password"></input>
+        </div>
     )
 }
 
