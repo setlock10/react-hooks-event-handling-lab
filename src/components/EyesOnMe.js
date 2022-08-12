3225d157ed6095cda94de7e3200fc36b516e56be
John J Setlock II <johnsetlock@gmail.com>
2022-08-12 07:35:09 -0500

    commit tree cb7752219a76b0961761802864164c9950a778a0

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe (){
+    function handleBlur(){
+        console.log("Hey! Eyes on me!")
+    }
+
+    function handleFocus(){
+        console.log('Good!')
+    }
+
+
+
+    return (
+        <div>
+            <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me.</button>
+        </div>
+    )
+}
+export default EyesOnMe;
